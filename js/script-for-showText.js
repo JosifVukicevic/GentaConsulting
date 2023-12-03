6ce9166b73c203317cbd5b2b71518f4819bbebb8
JosifVukicevic <josifjole32@gmail.com>
2023-12-03 18:05:08 +0100

    Blizimo se kraju
    Ostalo:
    -da se odrade ikonice,
    -slider za kompanije (partnere)
    -zamijeniti vertikalne slike iz slidera horizontalnim
    -dopuniti poslednji blog
    -dodati ostale blogove
    -promijeniti dizajn dugmadi na kockice  za usluge

---
+++ js/script-for-showText.js
@@ -0,0 +1,6 @@
+var text = document.getElementById("hiddenText-forAbout");
+var button = document.getElementById("buttonText");
+
+button.addEventListener("click", function() {
+    text.style.display = "block";
+  });
